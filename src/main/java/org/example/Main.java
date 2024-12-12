1adb7f1a732c635c34a832ce840aa1aa53fca857
Blake Velemirovich <Blake.Velemirovich@gmail.com>
2024-12-12 14:38:14 -0400

    Initial Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/*
+    Blake Velemirovich
+    2024-11-12
+    Default (Template) Project
+    org.example    
+*/public class Main {
+    public static void main(String[] args)  {
+        GameFrame frame = new GameFrame();
+    }
+}
---
+++ src/main/java/org/example/SnakeGame.java
@@ -0,0 +1,10 @@
+package org.example;/*
+    Blake Velemirovich
+    2024-11-12
+    Snake
+    org.example    
+*/
+
+public class SnakeGame {
+    GameFrame frame = new GameFrame();
+}
